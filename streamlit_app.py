8366a18f008b723aa8ef1cfab0707307f69ca377
CarlMorseAtBCI <154264612+CarlMorseAtBCI@users.noreply.github.com>
2023-12-18 11:21:26 -0700

    Update streamlit_app.py
    Subtitle to header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import streamlit
+erimport streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subtitle('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
